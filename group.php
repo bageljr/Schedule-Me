03fc60ee687b178b0a9b24f6242dd22b00647a9d
cpowers <connor.powers13@yahoo.com>
2017-12-03 14:13:56 -0500

    Type cast

--- group.php
+++ group.php
@@ -208,11 +208,13 @@
 				}
 			}
 		</script>
+		
 		<script>
 		function colorSpace(numUsers, j, numEvents) {
 			var color = numEvents/numUsers;
 			color = color * 10;
 			color = Math.round(color);
+			color = Math.trunc(color);
 			document.getElementById(j).classList.add('selected'+color);
 		}
 		</script>
